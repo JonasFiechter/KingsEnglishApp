7c35bb0482af0d34325fb17d0dac732c18a2697d
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-27 13:23:49 -0300

    layout update

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Second part!</Text>
+      </View>
+      <View style={styles.menu}>
+        <Text>Third part!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: StatusBar.height ? Platform.OS === 'android': 0,
+    backgroundColor: '#646',
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    backgroundColor: '#451',
+    flex: 1,
+    flexGrow: 0.1,
+  },
+  body: {
+    backgroundColor: '#2f1',
+    flex: 1,
+    flexGrow: 1,
+  },
+  menu: {
+    backgroundColor: '#21f',
+    flex: 1,
+    flexGrow: 0.1,
+  }
 });
