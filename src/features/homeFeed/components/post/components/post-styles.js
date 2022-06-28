5e8ce71b1c13ae6a60715969dbe4eedf02242b15
JonasFiechter <jonasrfiechter@gmail.com>
2022-06-28 14:59:43 -0300

    styling the post object

--- src/features/homeFeed/components/post/components/post-styles.js
+++ src/features/homeFeed/components/post/components/post-styles.js
@@ -11,7 +11,19 @@ export const PostCover = styled(Card.Cover)`
     padding-botton: ${(props) => props.theme.space[2]};
 `;
 
+export const PostTitle = styled.View`
+    flex: 1;
+    padding-top: ${(props) => props.theme.space[2]};
+`;
+
 export const PostDescription = styled.View`
+    padding-top: ${(props) => props.theme.space[2]};
+    padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+export const PostFooter = styled.View`
+    flex: 1;
+    flex-direction: row;
     padding-top: ${(props) => props.theme.space[2]};
     padding-bottom: ${(props) => props.theme.space[2]};
 `;
--- src/features/homeFeed/components/post/components/post.js
+++ src/features/homeFeed/components/post/components/post.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Text } from "../../../../../components/typography/textComponent.js";
-import { PostCard, PostCover, PostDescription } from './post-styles.js';
+import { PostCard, PostCover, PostDescription, PostFooter, PostTitle } from './post-styles.js';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export const Post = ({ post={} }) => {
     const {
         title = 'Post Title',
+        description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        author = 'Standard Custom Author',
         cover = ['https://images.pexels.com/photos/12081169/pexels-photo-12081169.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940']
     } = post;
 
     return (
         <PostCard>
+            <PostTitle>
+                <Text variant='title'>{title}</Text>
+            </PostTitle>
             <PostCover source={{ uri: cover[0] }}/>
             <PostDescription>
-                <Text variant='label'>{title}</Text>
+                <Text variant='body'>{description}</Text>
             </PostDescription>
+            <PostFooter>
+                <Text variant='caption'>{author}</Text>
+                <Ionicons 
+                    name={'md-home'} 
+                    size={2} 
+                    color={'gray'}/>
+            </PostFooter>
         </PostCard>
     );
 };
