fca02aa11710eaabdd080f5a8ee707bfeb6aa3c5
JonasFiechter <jonasrfiechter@gmail.com>
2022-08-09 19:32:18 -0300

    firebaseRegistering OK
    ...

--- src/services/authentication/firebase.config.js
+++ src/services/authentication/firebase.config.js
@@ -1,7 +1,7 @@
 //  Here we store some variables and the config that we will need to make the API run.
 
 import {initializeApp} from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 export const firebaseConfig = {
 	apiKey: "AIzaSyB79F1ZoHzBrEw4t2IEVC_DQjh5AhitlUE",
@@ -12,5 +12,5 @@ export const firebaseConfig = {
 	appId: "1:464719863448:web:955661b1fe2c889b747aed"
 	};
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const authentication = getAuth(app);
