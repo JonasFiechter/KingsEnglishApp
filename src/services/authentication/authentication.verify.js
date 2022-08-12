a928227a80a72c3322856a5407a570bec3924e86
JonasFiechter <jonasrfiechter@gmail.com>
2022-08-12 16:55:13 -0300

    .
    .

--- src/services/authentication/authentication.verify.js
+++ src/services/authentication/authentication.verify.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { AuthenticationContext } from "./authentication.context";
 
-
-export function verifyRegisterCredentials(credentials) {
+//  This module should receive the credentials for authentication and return any kind of typo error
+// or correct then right away if they are uppercase on email or some space or anything like that
+export function verifyCredentials(credentials) {
     console.log(credentials)
 }
